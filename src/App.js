81fe97847045f8a238b0434b785a0284808603cf
avi-technomads <avi.patel@technomads.in>
2022-08-10 14:12:11 +0530

    dummy voomio

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { Routes, Route } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Aggregator from "./Components/Aggregator/Aggregator";
+import NftGenerator from "./Components/NftGenerator/NftGenerator";
+import UploadNft from "./Components/UploadNft/UploadNft";
+import Preview from "./Components/UploadNft/Preview";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/aggregator" element={<Aggregator />} />
+        <Route exact path="/nftgenerator" element={<NftGenerator />} />
+        <Route exact path="/upload-nft" element={<UploadNft />} />
+        <Route exact path="/preview" element={<Preview/>}></Route>
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
